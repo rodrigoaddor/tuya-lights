a3f6152c457665645eb2a1e36598b6f09a485f78
Rodrigo Addor <me@rodr.dev>
2024-02-07 02:29:35 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,38 @@
+import TuyAPI from "tuyapi";
+import config from "./config.json" with { type: "json" };
+
+const tuya = new TuyAPI({
+  id: config.id,
+  ip: config.ip,
+  key: config.key,
+  version: config.version,
+});
+
+await tuya.find();
+await tuya.connect();
+
+const properties = await tuya.get({
+  schema: true,
+  dps: config.source,
+});
+
+const status = properties.dps[config.source];
+
+const value = !status;
+
+await tuya.set({
+  multiple: true,
+  data: config.targets.reduce((obj, target) => {
+    obj[target] = true;
+    return obj;
+  }, {}),
+});
+
+// for (const target of config.targets) {
+//     await tuya.set({
+//         set: value,
+//         dps: target
+//     })
+// }
+
+tuya.disconnect();
